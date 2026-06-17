95e84f5036357e127a4f9e2cc057a382ec62f80a
sixdgt <c4crypt@gmail.com>
2026-06-17 21:12:32 +0545

    fix: docker files & api

--- frontend/app/store/useWishlistStore.ts
+++ frontend/app/store/useWishlistStore.ts
@@ -1,6 +1,6 @@
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
-import api from '../lib/api';
+import api from '@/app/lib/api';
 
 interface WishlistState {
   items: any[];
