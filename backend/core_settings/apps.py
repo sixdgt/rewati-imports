0d248db033fc616eb8b957dfc8af6f80667d889c
sixdgt <c4crypt@gmail.com>
2026-06-21 16:00:48 +0545

    fix: core setting

--- backend/core_settings/apps.py
+++ backend/core_settings/apps.py
@@ -6,6 +6,9 @@ class CoreSettingsConfig(AppConfig):
     default = True
     name = 'core_settings'
 
+    def ready(self):
+        import core_settings.admin
+        
 class CustomAdminConfig(django.contrib.admin.apps.AdminConfig):
     default = False
     default_site = 'core_settings.admin.CustomAdminSite'
